9ebc41da9bac50df34b26ec293acd887f3402f94
Krishna Kumar <kkumar@exathink.com>
2026-02-07 10:09:23 -0600

    Added eleventy.js

---
+++ v1/.eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
+
+  // Watch CSS files for changes
+  eleventyConfig.addWatchTarget("src/css/");
+
+  // Set custom directories
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"]
+  };
+};
